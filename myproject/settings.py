8195062ae522bb51dbc8d836be1d12c161345717
Thony-Leca <83093101+Thony-Leca@users.noreply.github.com>
2023-07-17 19:31:59 -0500

    a

--- myproject/settings.py
+++ myproject/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8mau*#txynfuhwwmr6rmr7&do&a+98xeos)wc3j=$&o*pq)#^o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
